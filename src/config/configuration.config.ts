505aeb45092aab6a4777e3534ff88dc31a7df4cf
nqam <anhminh.nghiem@wejelly.com>
2021-09-29 12:08:18 +0700

    make it better

--- src/config/configuration.config.ts
+++ src/config/configuration.config.ts
@@ -1,23 +1,23 @@
 /* eslint-disable prettier/prettier */
-// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-// export const typeOrmConfig: TypeOrmModuleOptions = {
-//    type: 'postgres',
-//    host: 'bnj8hnlrjflgc7b26sov-postgresql.services.clever-cloud.com',
-//    port: 5432,
-//    username: 'usfyqrimfphp3tiqiokr',
-//    password: '3uCnZtDaIGHGp7Bth2lO',
-//    database: 'bnj8hnlrjflgc7b26sov',
-//    entities: ['dist/**/*.entity{.ts,.js}'],
-//    synchronize: true,
-// };
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
    type: 'postgres',
-   host: 'localhost',
+   host: 'bnj8hnlrjflgc7b26sov-postgresql.services.clever-cloud.com',
    port: 5432,
-   username: 'postgres',
-   password: '123456',
-   database: 'books',
+   username: 'usfyqrimfphp3tiqiokr',
+   password: '3uCnZtDaIGHGp7Bth2lO',
+   database: 'bnj8hnlrjflgc7b26sov',
    entities: ['dist/**/*.entity{.ts,.js}'],
    synchronize: true,
 };
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//    type: 'postgres',
+//    host: 'localhost',
+//    port: 5432,
+//    username: 'postgres',
+//    password: '123456',
+//    database: 'books',
+//    entities: ['dist/**/*.entity{.ts,.js}'],
+//    synchronize: true,
+// };
